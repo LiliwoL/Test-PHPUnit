bda68421c711d2f3543245e639042676634ce0da
LiliwoL <github@liliwol.fr>
2023-04-05 11:57:43 +0200

    First

---
+++ PasswordTest.php
@@ -0,0 +1,20 @@
+<?php
+
+include('vendor/autoload.php');
+
+use PHPUnit\Framework\TestCase;
+
+
+function verifPassword($mdp): bool
+{
+}
+
+
+
+
+class PasswordTest extends TestCase
+{
+    public function testVerifPassword()
+    {
+    }    
+}
